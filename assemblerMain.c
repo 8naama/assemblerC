099aa9bb82ddafaba8ff3d3e231ba622e724dbfa
8naama <naama.bendalak@logz.io>
2024-04-03 16:51:40 +0300

    add macro to the main run

--- assemblerMain.c
+++ assemblerMain.c
@@ -1,22 +1,68 @@
 #include "dataStructures.h"
 #include "firstScan.h"
+#include "macro.h"
 #include "filesGenerator.h"
+#include <string.h>
 
 
-#define EXPECTED_FILE_EXTENSION ".as"
+#define ORIGINAL_FILE_EXTENSION ".as"
 
 
 /*
 This main function is temporary for test purpose.
 */
-int main()
+int main(int argc, char *argv[])
 {
-    int succesful_scan = 0;
-    /* call macro() here to create the .am file */
+    int succesful_scan = 0, i, filenameLen;
+    char *filename, *fileExtension, *filenameNoExtention;
+    FILE * fd;
 
-    succesful_scan = firstScan("test");
 
-    printSymbols();
+    if (argc == 1) {
+        printf("Error: Please enter at least one %s file as argument\n", ORIGINAL_FILE_EXTENSION);
+        exit(1);
+    }
+
+    /* verify the filename length, extension type and permissions */
+    for (i = 1; i < argc; i++) {
+        filename = argv[i];
+
+        if (strlen(filename) < 4) {
+            printf("Error: provided filename %s is too short.\n", filename);
+            exit(1);
+        }
+        else {
+            fileExtension = &filename[strlen(filename)-3];
+
+            if (strcmp(fileExtension, ORIGINAL_FILE_EXTENSION) != 0) {
+                printf("ERROR: file %s has extension %s but expected %s\n", filename, fileExtension, ORIGINAL_FILE_EXTENSION);
+                exit(1);
+            }
+        }
+        if (!(fd = fopen(filename, "r"))) {
+            fprintf(stderr, "ERROR: File %s does not exist or missing read permissions\n", filename);
+            exit(1);
+        }
+    }
+
+    for (i = 1; i < argc; i++) {
+        filename = argv[i];
+
+        filenameLen = strlen(filename);
+        filenameNoExtention = (char *) malloc(filenameLen+1);
+        strncpy(filenameNoExtention, filename, filenameLen - 3);
+        filenameNoExtention[filenameLen - 3] = '\0';
+
+
+        spreadMacros(filenameNoExtention);
+        succesful_scan = firstScan(filenameNoExtention);
+
+        printSymbols();
+
+        if (succesful_scan == 0)
+            printf("call second scan");
+            /* call second scan here */
+    }
 
     return succesful_scan;
 }
--- macro.h
+++ macro.h
@@ -3,18 +3,14 @@
 #include <string.h>
 #include <ctype.h>
 #define MAX 100
+#define INPUT_FILE_TYPE ".as"
+#define OUTPUT_FILE_TYPE ".am"
+
 
 struct Macro {
     char name[MAX];
     char content[MAX];
     struct Macro* next;
 };
-/*void addToMtable(struct Macro *head , char name[] , char content[]);
-void printMacroTable(struct Macro *tail);*/
-int isMacro(char line[]);
-void insertName(struct Macro *pMcr, char line[]);
-void insertContent(struct Macro *pMcr, FILE *fpr);
-int isMacroCommand(char line[], FILE *fpw, struct Macro *mTail);
-int readFile(int i, char *argv[], struct Macro *mHead);
-void checkLine(FILE *fpw, struct Macro *tail, char *line);
-void writeFile(int i, char *argv[],struct Macro *mTail);
+
+void spreadMacros(char filename[]);
